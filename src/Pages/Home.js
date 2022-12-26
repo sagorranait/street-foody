ce3590fa1007701e56d706c45ca6fd6f06442643
Sagor Rana <sagorranait@gmail.com>
2022-12-26 10:19:22 +0600

    Slider design is done.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,13 @@
 import { Col, Container, Row } from "react-bootstrap";
-import fuchka from '../assets/fuchka.jpg';
 import { BsArrowRight } from "react-icons/bs";
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
+import fuchka from '../assets/fuchka.jpg';
+import fuchkaThum from '../assets/fuchka-thum.jpg';
 
 function Home() {
   const settings = {
     className: "left",
-    centerMode: true,
     infinite: true,
     centerPadding: "60px",
     slidesToShow: 3,
@@ -20,33 +20,39 @@ function Home() {
       <div className="content">
         <Container>
             <Row>
-              <Col sm={12} md={6} lg={6} xl={6}>
+              <Col sm={12} md={6} lg={4} xl={4}>
                   <div className="category-content">
                       <h1>Fuchka</h1>
                       <p>This Fuchka is usually served with tamarinds filled with spices filled with spicy potatoes in a round petal prepared by the flour and suji.</p>
                       <button className='streetFood-btn'>Browse All <span><BsArrowRight/></span></button>
                   </div>
               </Col>
-              <Col sm={12} md={6} lg={6} xl={6}>
+              <Col sm={12} md={6} lg={8} xl={8}>
                 <Slider {...settings}>
                   <Link to='/' className="category-card">
-                    <span>Fuchka</span>
+                    <div className="card-content" style={{backgroundImage: `url(${fuchkaThum})`}}>
+                        <div className="card-overlay"></div>
+                        <span>Fuchka</span>
+                    </div>
                   </Link>
-                  <div>
-                    <h3>2</h3>
-                  </div>
-                  <div>
-                    <h3>3</h3>
-                  </div>
-                  <div>
-                    <h3>4</h3>
-                  </div>
-                  <div>
-                    <h3>5</h3>
-                  </div>
-                  <div>
-                    <h3>6</h3>
-                  </div>
+                  <Link to='/' className="category-card">
+                    <div className="card-content" style={{backgroundImage: `url(${fuchkaThum})`}}>
+                        <div className="card-overlay"></div>
+                        <span>Fuchka1</span>
+                    </div>
+                  </Link>
+                  <Link to='/' className="category-card">
+                    <div className="card-content" style={{backgroundImage: `url(${fuchkaThum})`}}>
+                        <div className="card-overlay"></div>
+                        <span>Fuchka2</span>
+                    </div>
+                  </Link>
+                  <Link to='/' className="category-card">
+                    <div className="card-content" style={{backgroundImage: `url(${fuchkaThum})`}}>
+                        <div className="card-overlay"></div>
+                        <span>Fuchka3</span>
+                    </div>
+                  </Link>
                 </Slider> 
               </Col>
             </Row>
