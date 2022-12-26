3ed6a6efc09cd52890d65fdd47f847aa9285fc58
Sagor Rana <sagorranait@gmail.com>
2022-12-27 00:55:31 +0600

    Design of Register page is done.

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,12 +1,28 @@
-import { Container } from 'react-bootstrap'
+import { Link } from "react-router-dom"
+import Forms from "../Components/Forms"
 
 function Register() {
+   const registerHandler = () => {
+
+   }
+
   return (
    <div id="main">
       <div className="main-content">
-         <Container>
-            register
-         </Container>
+         <Forms 
+            title="Create an Account" 
+            description="Even though registration just takes a few seconds, it provides you complete control over your purchase."
+            toastMessage="Sign-Up Successfully."
+         >
+            <form className='user-form' onSubmit={registerHandler}>
+            <input type="text" name="name" id="nameInput" placeholder='Username' required />
+            <input type="email" name="email" id="emailInput" placeholder='Email' required />
+            <input type="password" name="password" id="passwordInput" placeholder='Password' required />
+            <input type="password" name="confrimPassword" id="passwordInput" placeholder='Confrim Passowrd' required />
+            <button type="submit" className='streetFood-btn'>Register</button>
+            <p>Already have an account? <Link to='/login'>Login</Link></p>
+            </form>
+         </Forms>
       </div>
    </div>
   )
